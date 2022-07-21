8d5045fd034e2352faa4355988c24362efc8b834
Revanth Chimmani <revanthchimmani@gmail.com>
2022-07-20 22:36:05 -0700

    Initial Setup

---
+++ src/test/java/com/revanth/letschat/LetschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revanth.letschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
